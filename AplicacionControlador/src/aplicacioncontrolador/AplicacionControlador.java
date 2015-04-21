592c517f759f2fedaef87113e65434560a14e1a8
lozadaerika <lozada.m.erika@gmail.com>
2015-04-21 18:04:16 -0500

    Update AplicacionControlador.java

--- AplicacionControlador/src/aplicacioncontrolador/AplicacionControlador.java
+++ AplicacionControlador/src/aplicacioncontrolador/AplicacionControlador.java
@@ -16,6 +16,7 @@ public class AplicacionControlador {
      */
     public static void main(String[] args) {
         System.out.println("Erika Lozada");
+        System.out.println("Modificado online");
     }
     
 }
